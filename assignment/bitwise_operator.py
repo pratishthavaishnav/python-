f85741964e159d74691e1ce00d81c074977272c3
Pratishtha vaishnav <pratishthavaishnav652@gmail.com>
2026-05-22 20:44:24 +0530

    new operator

--- assignment/bitwise_operator.py
+++ assignment/bitwise_operator.py
@@ -4,12 +4,37 @@ WRITE_PERMISSION = 2  #010
 EXECUTE_PERMISSION = 1  #001
 
 user_permission = int(input("Enter user permission (0-7): "))
-required_permission = 7
 
-if (user_permission & required_permission) == required_permission:
+required_permission = READ_PERMISSION & WRITE_PERMISSION & EXECUTE_PERMISSION
+if user_permission & required_permission == required_permission:
     print("Permission granted.")
 else:
     print("Permission denied.")
 
 
 #27.Write a Python program to toggle system settings using XOR.
+camera_setting = int(input("Enter current camera setting (0-15): "))
+toggle_camera = int(input("Enter toggle camera value (0-15): "))
+
+new_camera_setting = camera_setting ^ toggle_camera
+print("camera setting after toggle:", new_camera_setting)
+
+
+#28.Write a Python program to double a number using left shift.
+number = 6
+doubled_number = number << 1
+print("doubled number:", doubled_number)
+
+
+#29.Write a Python program to divide a number by 2 using right shift.
+number = 10 
+divided_number = number >> 2 
+print("divided number:", divided_number)
+
+
+#30.Write a Python program to check even or odd using bitwise AND.
+number = 7
+if number & 1:
+    print("Number is odd.")
+else:
+    print("Number is even.")
