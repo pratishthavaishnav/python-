bf1ee18ea2d387e59065ddb1a7ff2bdd68aa67cf
Pratishtha vaishnav <pratishthavaishnav652@gmail.com>
2026-05-22 20:20:23 +0530

    new operator

--- assignment/bitwise_operator.py
+++ assignment/bitwise_operator.py
@@ -1,2 +1,15 @@
 #26.Write a Python program to perform permission check using bitwise AND.
-permission = input()
+READ_PERMISSION = 4  #100
+WRITE_PERMISSION = 2  #010
+EXECUTE_PERMISSION = 1  #001
+
+user_permission = int(input("Enter user permission (0-7): "))
+required_permission = 7
+
+if (user_permission & required_permission) == required_permission:
+    print("Permission granted.")
+else:
+    print("Permission denied.")
+
+
+#27.Write a Python program to toggle system settings using XOR.
