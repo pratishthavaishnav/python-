4a394ffacfa11fd484b90abc6977b570540628d7
Pratishtha vaishnav <pratishthavaishnav652@gmail.com>
2026-05-24 01:11:59 +0530

    new codes

--- 23-05-26/program5.py
+++ 23-05-26/program5.py
@@ -3,14 +3,23 @@ cat: furniture
 state: california
 calculate total profit, quantity , sales
 '''
-
 #import required library
 import openpyxl
 
-#load workbook
-wb = openpyxl.load_workbook("C:\Users\DELL\OneDrive\Desktop\python\dataset\Superstore-1.xlsx")
-
+# load workbook
+wb = openpyxl.load_workbook(r"C:\Users\DELL\OneDrive\Desktop\python\Superstore-1.xlsx")
 sheet = wb["Orders"]
 
-for data in sheet.iter_rows(max_row=3, values_only=True):
-    print(data[14])
+total_profit = 0
+total_quantity = 0
+total_sales = 0
+
+for data in sheet.iter_rows(min_row=2, values_only=True):
+    if data[14] == "Furniture" and data[10] == "California":
+        total_profit += data[20] or 0
+        total_quantity += data[18] or 0
+        total_sales += data[17] or 0
+
+print(f"Total profit: {total_profit}")
+print(f"Total quantity: {total_quantity}")
+print(f"Total sales: {total_sales}")
---
+++ 23-05-26/whileloop.py
@@ -0,0 +1,10 @@
+num1= 5
+num2= 12
+
+while True:
+    if(max % num1 == 0 and max % num2 == 0):
+        print("LCM",max)
+        break
+    
+    else:
+        max += 1
